7669d5056504a8f4c3a84c8e677854e38bcb1c46
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-07-27 00:30:18 -0500

    Changed project manager folder name.

--- admin/urls.py
+++ admin/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ProjectS4 project.
+URL configuration for admin project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
