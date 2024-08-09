3b81000086855316c6bae4ffb29809c46c7128b0
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-08-09 01:07:07 -0500

    Created model - urls manager. Added url and view to call the roadmap generator.

--- roadMap/views.py
+++ roadMap/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from admin.openAIManager import openAIManager
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def roadmapGenerator(request):
+    bot = openAIManager()
+    bot.generateRoadmap("software engineeer", "software engineer at google", 100000)
+    # if request.method == 'POST':
+    #     return render(request, 'roadmapGenerator.html')
+    return render(request, 'index.html')
