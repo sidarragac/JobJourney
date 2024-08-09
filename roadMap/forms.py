94d9f70aca9ca01045b609596c27cdc69a33ffb5
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-08-09 01:07:56 -0500

    Created basic form template to receive interest, objectives and salary (roadmap generation).

---
+++ roadMap/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class roadmapCharacteristics(forms.Form):
+    interest = forms.CharField(label='Interest', max_length=100, required=True)
+    objective = forms.CharField(label='Objective', max_length=100, required=True)
+    salary = forms.FloatField(label='Salary', min_value=0.0, required=False)
