6ef8bc5aa3afc68ad1cbd7fe477e8e9c030481b9
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-08-10 03:13:10 -0500

    Updated url instruction for page 1st load.

--- roadMap/urls.py
+++ roadMap/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.interestForm, name='interestForm'),
     path('roadmapGenerator', views.roadmapGenerator, name='roadmapGenerator'),
 ]
