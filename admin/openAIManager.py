14c7a609131d98bad87109e4e685c175aaf6fa20
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-08-14 00:53:08 -0500

    Updated roadmap generation prompt and activated the OpenAI requests method.

--- admin/openAIManager.py
+++ admin/openAIManager.py
@@ -12,7 +12,7 @@ class openAIManager():
         content = f"""Imagine you're an experienced recruiter. You're requested to build a 5 steps, self-study roadmap to be a {objective}. For each step of the roadmap, bring recommendations on how to expand the topic you're suggesting and some material you can provide. Also, provide a job I might apply for after completing this goal. Justify how this roadmap will benefy me on my professional career and finally, after making an analysis, say if {salary} dollars a year as expected salary is a good estimation, or either to high or to low considering the level that can be achieved with this roadmap.
 
 Bring your answer in the following json format:
-name: -roadmap to become- what I want to become.,
+name: -Roadmap to become- what I want to become.,
 steps: array of steps in the following format:
 -number: step number,
 -name: step name,
