f1cf8892bbe0e255bb724d526d2066ca477d41ba
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-08-13 20:49:41 -0500

    Added example to build html template, and modified views file to use the example instead of the API.

--- roadMap/views.py
+++ roadMap/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from admin.openAIManager import openAIManager
+import json
 
 def interestForm(request):
     return render(request, 'interestForm.html')
 
 def roadmapGenerator(request):
-    if request.method == 'POST':
-        interest = request.POST.get('interest')
-        objective = request.POST.get('objective')
-        salary = request.POST.get('salary')
-        bot = openAIManager()
-        roadmap = bot.generateRoadmap(objective=objective, salary=salary) #JSON with detailed roadmap.
-        return render(request, 'roadmap.html', {'roadmap': roadmap})
-    else:
-        return render(request, 'interestForm.html')
+    # if request.method == 'POST':
+        # interest = request.POST.get('interest')
+        # objective = request.POST.get('objective')
+        # salary = request.POST.get('salary')
+        # bot = openAIManager()
+        # roadmap = bot.generateRoadmap(objective=objective, salary=salary) #JSON with detailed roadmap.
+    f = open("./roadMap/roadmap.json")
+    roadmap = json.loads(f.read())
+    return render(request, 'roadmap.html', {'roadmap': roadmap})
+    # else:
+    #     return render(request, 'interestForm.html')
