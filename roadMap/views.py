087e931f102185353e7721292014f3ee619191ef
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-08-14 10:54:52 -0500

    Added form validation

--- roadMap/views.py
+++ roadMap/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from admin.openAIManager import openAIManager
+from forms import *
 import json
 
 def interestForm(request):
@@ -7,14 +8,18 @@ def interestForm(request):
 
 def roadmapGenerator(request):
     if request.method == 'POST':
-        interest = request.POST.get('interest')
-        objective = request.POST.get('objective')
-        salary = request.POST.get('salary')
-        bot = openAIManager()
-        roadmap = bot.generateRoadmap(objective=objective, salary=salary) #JSON with detailed roadmap.
-        return render(request, 'roadmap.html', {'roadmap': roadmap})
-        # f = open("./roadMap/roadmap.json")
-        # roadmap = json.loads(f.read())
-        # return render(request, 'roadmap.html', {'roadmap': roadmap})
+        form = RoadmapCharacteristics(request.POST)
+        if form.is_valid():
+            interest = form.cleaned_data['interest']
+            objective = form.cleaned_data['objective']
+            salary = form.cleaned_data['salary']
+            bot = openAIManager()
+            roadmap = bot.generateRoadmap(objective=objective, salary=salary) #JSON with detailed roadmap.
+            return render(request, 'roadmap.html', {'roadmap': roadmap})
+            # f = open("./roadMap/roadmap.json")
+            # roadmap = json.loads(f.read())
+            # return render(request, 'roadmap.html', {'roadmap': roadmap})
+        else:
+            return render(request, 'interestForm.html', {"message": "Some provided data are not valid."})
     else:
         return render(request, 'interestForm.html')
