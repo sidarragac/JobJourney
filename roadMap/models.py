11a538f445b8d239ff4ffd640ed1bf670eb667fb
JuanJoseBotero <jjboterog@eafit.edu.co>
2024-08-30 00:37:51 -0500

    Creation of models of roadmap app and migrations

--- roadMap/models.py
+++ roadMap/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class Interest(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+class RoadMap(models.Model):
+    mainGoal = models.CharField(max_length=100)
+    content = models.JSONField()
+    completionPercentage = models.IntegerField()
+    idUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    idInterest = models.ForeignKey(Interest, on_delete=models.CASCADE)
