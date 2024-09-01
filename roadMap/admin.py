d19b262d6ce7b2e731c4714cfc96505837073688
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-09-01 18:59:15 -0500

    Updated Models and registered apps

--- roadMap/admin.py
+++ roadMap/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Roadmap)
+admin.site.register(Interest)
+admin.site.register(Checkpoint)
--- roadMap/models.py
+++ roadMap/models.py
@@ -1,19 +1,28 @@
 from django.db import models
-from accounts.models import User
+from accounts.models import Person
 
 # Create your models here.
 class Interest(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
 
-class RoadMap(models.Model):
+    def __str__(self):
+        return self.name
+
+class Roadmap(models.Model):
     mainGoal = models.CharField(max_length=100)
     content = models.JSONField()
     completionPercentage = models.IntegerField()
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(Person, on_delete=models.CASCADE)
     idInterest = models.ForeignKey(Interest, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.mainGoal
 
 class Checkpoint(models.Model):
     step = models.CharField(max_length=100)
     checked = models.BooleanField()
-    idRoadMap = models.BooleanField()
+    idRoadmap = models.ForeignKey(Interest, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.step
