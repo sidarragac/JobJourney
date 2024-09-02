e62ca297d0abfe35a049b918bd79e2fc51f0e5b0
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-09-02 01:35:28 -0500

    Implemented form to keep track of checkpoints

--- roadMap/urls.py
+++ roadMap/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.interestForm, name='interestForm'),
     path('roadmapGenerator', views.roadmapGenerator, name='roadmapGenerator'),
+    path('checkpointUpdate', views.checkpointUpdate, name='checkpointUpdate'),
 ]
--- roadMap/views.py
+++ roadMap/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from admin.openAIManager import openAIManager
+from django.contrib.auth.decorators import login_required
 from .forms import *
+from .models import *
+import asyncio
+import json
 
 def interestForm(request):
     return render(request, 'interestForm.html')
@@ -9,13 +13,27 @@ def roadmapGenerator(request):
     if request.method == 'POST':
         form = RoadmapCharacteristics(request.POST)
         if form.is_valid():
-            interest = form.cleaned_data['interest']
-            objective = form.cleaned_data['objective']
-            salary = form.cleaned_data['salary']
-            bot = openAIManager()
-            roadmap = bot.generateRoadmap(objective=objective, salary=salary) #JSON with detailed roadmap.
-            return render(request, 'roadmap.html', {'roadmap': roadmap})
+            # interest = form.cleaned_data['interest']
+            # objective = form.cleaned_data['objective']
+            # salary = form.cleaned_data['salary']
+            # bot = openAIManager()
+            # roadmap = bot.generateRoadmap(objective=objective, salary=salary) #JSON with detailed roadmap.
+
+            # instanciar un obj del modelo roadmap
+
+            f = open("./roadMap/roadmap.json")
+            roadmap = json.loads(f.read())
+
+            return render(request, 'roadmap.html', {'roadmap': roadmap, 'roadmapID': 1})
         else:
             return render(request, 'interestForm.html', {"message": "Some provided data are not valid."})
     else:
         return render(request, 'interestForm.html')
+    
+async def __updateCheckpointStatus(checkpoint, roadmap, status):
+    Checkpoint.objects.filter(numberOfCheckpoint=checkpoint, idRoadmap=roadmap).update(completed=status)
+
+@login_required
+def checkpointUpdate(request):
+
+    return render(request, 'completedCheckpoint.html')
