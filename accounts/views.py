1d295749325c35d945ff84d87a6714f8eef74026
Mateo Garcia Carreno <mgarciac03@outlook.com>
2024-09-03 18:18:05 -0500

    Working login and registration of models Person and Company done

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 from .models import *
 
 # Create your views here.
-def login(request):
+def loginView(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST.get('username'), password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            return redirect('interestForm')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
     return render(request, 'login.html')
 
-def register(request):
+def registerView(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        print(request.POST)
         if form.is_valid():
             user = form.save()
-            """ if user.isCompany:
+            if user.isCompany:
                 companyName = request.POST.get('companyName')
                 Company.objects.create(user=user, companyName=companyName)
             else:
                 dateOfBirth = request.POST.get('dateOfBirth')
-                Person.objects.create(user=user, dateOfBirth=dateOfBirth) """
+                Person.objects.create(user=user, dateOfBirth=dateOfBirth)
             return redirect('login')
-        print(form.errors)
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
