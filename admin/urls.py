e63eb0d755311c1a60acea6f77fdf674cec0989b
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-09-10 01:25:54 -0500

    Added home url.

--- admin/urls.py
+++ admin/urls.py
@@ -20,6 +20,7 @@ from roadMap.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home,  name='home'),
     path('roadmap/', include('roadMap.urls')),
     path('accounts/', include('accounts.urls')),
     path('analytics/', include('analytics.urls')),
