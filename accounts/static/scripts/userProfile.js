99ed75568c4f054c7a40baa4620a893e51744224
JuanJoseBotero <jjboterog@eafit.edu.co>
2024-11-06 23:59:45 -0500

    Add close button to editProfile section

--- accounts/static/scripts/userProfile.js
+++ accounts/static/scripts/userProfile.js
@@ -1,19 +1,33 @@
-const settingButton = document.getElementById("settingsButton");
-const modal = document.getElementById("modal");
-
-settingButton.addEventListener("click",() => {
-    modal.showModal();
-});
-
-let profilePic = document.getElementById("profilePic");
-let inputFile = document.getElementById("inputFile");
-
-inputFile.onchange = function() {
-    profilePic.src = URL.createObjectURL(inputFile.files[0]);
+window.onload = function() {
+    modalSettings();
 }
+
+function modalSettings() {
+    const settingButton = document.getElementById("settingsButton");
+    const modal = document.getElementById("modal");
+    const closeButton = document.getElementById("closeButton");
+
+    settingButton.addEventListener("click",() => {
+        modal.showModal();
+    });
+    closeButton.addEventListener("click",() => {
+        modal.close();
+    });
+}
+
+function savePicture() {
+    let profilePic = document.getElementById("profilePic");
+    let inputFile = document.getElementById("inputFile");
+    
+    inputFile.onchange = function() {
+        profilePic.src = URL.createObjectURL(inputFile.files[0]);
+    }
+}
+
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.close();
     }
 }
+
