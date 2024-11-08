ef6292f65e167d7e3602cbdf0cd1713bafdb2b23
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-11-08 02:50:01 -0500

    Fixed bug with roadmap's name language

--- admin/openAIManager.py
+++ admin/openAIManager.py
@@ -15,7 +15,7 @@ class openAIManager():
         content = f"""Imagine you're an experienced recruiter. You're requested to build a 5 steps, self-study roadmap to be a {objective}. For each step of the roadmap, bring recommendations on how to expand the topic you're suggesting and some material you can provide. Ensure that each step you bring is planned learn something towards the desired objective. Focus only in courses I can do from home, online. Also, provide a job I might apply for after completing each step. Justify how this roadmap will benefy me on my professional career and finally, after making an analysis, say if {salary} dollars a year as expected salary is a good estimation, or either to high or to low considering the level that can be achieved with this roadmap.
 
 Bring your answer in the following json format:
-name: -what I want to become-.,
+name: -what I want to become-. (If this is in another language, please translate it to english),
 steps: array of steps in the following format:
 -number: step number,
 -name: step name,
